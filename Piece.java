9e88b3841fad943e6c8af8ac8a09c0fb6c7fa65b
ChrisSwingle <swingle.chris@gmail.com>
2020-03-30 15:53:03 -0400

    piece super class

--- Piece.java
+++ Piece.java
@@ -1,2 +1,24 @@
 public class Piece {
+    public enum Color {white, black}
+    public enum Type {pawn, bishop, king, queen, rook, knight}
+
+    private int x;
+    private int y;
+    private Color color;
+    private Type type;
+
+    Piece(int x, int y, Color color, Type type){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.type = type;
+    }
+
+    public void set_x(int x){
+        this.x = x;
+    }
+
+    public void set_y(int y){
+        this.y = y;
+    }
 }
