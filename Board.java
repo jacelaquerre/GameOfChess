6fb8ba7ff19274041c232b0c07bf9e8be23a1574
jacelaquerre <Colchester31>
2020-04-10 20:51:43 -0400

    finished making board

--- Board.java
+++ Board.java
@@ -16,39 +16,33 @@ public class Board {
 
     public void create_board(){
 
-        // THE CORDINATES FOR EACH PIECE IS INCORRECT RIGHT NOW
-
-        //PAWNS
-       // for(int c = 0; c < 8; c++){
-       //     Piece pWhite = new Pawn(c, 1, Piece.Color.white, Piece.Type.pawn, true);
-       //     Piece pBlack = new Pawn(c, 6, Piece.Color.black, Piece.Type.pawn, true);
-       //     board[c][1] = pWhite;
-       //     board[c][6] = pBlack;
-       // }
-
-        //KING & QUEEN
-        //Piece kingWhite = new King(5,0, Piece.Color.white, Piece.Type.king, true);
-        //Piece queenWhite = new Queen(4,0, Piece.Color.white, Piece.Type.queen, true);
-        //Piece kingBlack = new King(4,7, Piece.Color.black, Piece.Type.king, true);
-        //Piece queenBlack = new Queen(5,7, Piece.Color.black, Piece.Type.queen, true);
-
-        // BISHOP
-        //Piece bishopWhiteA = new Bishop(2, 0, Piece.Color.white, Piece.Type.bishop, true);
-        //Piece bishopWhiteB = new Bishop(5, 0, Piece.Color.white, Piece.Type.bishop, true);
-        //Piece bishopBlackA = new Bishop(2, 7, Piece.Color.black, Piece.Type.bishop, true);
-       // Piece bishopBlackB = new Bishop(5, 7, Piece.Color.black, Piece.Type.bishop, true);
-
-        // ROOK
-        //Piece rookWhiteA = new Rook( 0, 0, Piece.Color.white, Piece.Type.rook, true);
-        //Piece rookWhiteB = new Rook(0,7, Piece.Color.white, Piece.Type.rook, true);
-        //Piece rookBlackA = new Rook(7,0, Piece.Color.black, Piece.Type.rook, true);
-        //Piece rookBlackB = new Rook(7,7, Piece.Color.black, Piece.Type.rook, true);
-
-        // KNIGHTS
+        // WHITE PLAYER
+        board[0][0] = new Box(new Rook(Piece.Color.white, true), 0, 0);
         board[0][1] = new Box(new Knight(Piece.Color.white, true), 0, 1);
-        //Piece knightWhiteB = new Knight(0,5, Piece.Color.white, Piece.Type.knight, true);
-        //Piece knightBlackA = new Knight(7,2, Piece.Color.black, Piece.Type.knight, true);
-        //Piece knightBlackB = new Knight(7,5, Piece.Color.black, Piece.Type.knight, true);
+        board[0][2] = new Box(new Bishop(Piece.Color.white, true), 0, 2);
+        board[0][3] = new Box(new Rook(Piece.Color.white, true), 0, 3);
+        board[0][4] = new Box(new Queen(Piece.Color.white, true), 0, 4);
+        board[0][5] = new Box(new King(Piece.Color.white, true), 0, 5);
+        board[0][6] = new Box(new Bishop(Piece.Color.white, true), 0, 6);
+        board[0][7] = new Box(new Knight(Piece.Color.white, true), 0, 7);
+        board[0][8] = new Box(new Rook(Piece.Color.white, true), 0, 8);
+         for(int i = 0; i < 8; ++i) {
+             board[1][i] = new Box(new Pawn(Piece.Color.white, true), 1, i);
+         }
+
+        // BLACK PLAYER
+        board[8][0] = new Box(new Rook(Piece.Color.black, true), 8, 0);
+        board[8][1] = new Box(new Knight(Piece.Color.black, true), 8, 1);
+        board[8][2] = new Box(new Bishop(Piece.Color.black, true), 8, 2);
+        board[8][3] = new Box(new Rook(Piece.Color.black, true), 8, 3);
+        board[8][4] = new Box(new Queen(Piece.Color.black, true), 8, 4);
+        board[8][5] = new Box(new King(Piece.Color.black, true), 8, 5);
+        board[8][6] = new Box(new Bishop(Piece.Color.black, true), 8, 6);
+        board[8][7] = new Box(new Knight(Piece.Color.black, true), 8, 7);
+        board[8][8] = new Box(new Rook(Piece.Color.black, true), 8, 8);
+        for(int i = 0; i < 8; ++i) {
+            board[7][i] = new Box(new Pawn(Piece.Color.black, true), 7, i);
+        }
 
         // Add empty spots
         for (int i = 2; i < 6; ++i) {
